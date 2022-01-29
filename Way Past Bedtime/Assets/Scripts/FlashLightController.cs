8d949eec73d8d19395730df006f0db760dc0bdbb
Gavin <gamerscience@yahoo.com>
2022-01-29 12:37:09 -0700

    separate player and flashlight rotation

--- Way Past Bedtime/Assets/Scripts/FlashLightController.cs
+++ Way Past Bedtime/Assets/Scripts/FlashLightController.cs
@@ -12,12 +12,18 @@ public class FlashLightController : MonoBehaviour
 
     private Controls defaultControls;
 
+    Rigidbody2D rb2d;
+    Camera cam;
+    Vector2 mousePos;
+
     bool active = false;
 
     private void Awake()
     {
         defaultControls = new Controls();
         flashlight.gameObject.SetActive(active);
+        rb2d = transform.parent.GetComponent<Rigidbody2D>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     private void OnEnable()
@@ -44,7 +50,15 @@ public class FlashLightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mousePos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - rb2d.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x)*Mathf.Rad2Deg + 90f;
+        transform.rotation = Quaternion.Euler(new Vector3(0,0,angle));
     }
 
 }
--- Way Past Bedtime/Assets/Scripts/PlayerMovement.cs
+++ Way Past Bedtime/Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 5;
 
     Camera cam;
-    Vector2 mousePos;
 
     public void OnPlayerMovement(InputAction.CallbackContext context)
     {
@@ -27,13 +26,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
     }
 
-    private void FixedUpdate()
-    {
-        Vector2 lookDir = mousePos - rb2d.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
-        rb2d.rotation = angle;
-    }
+    
 }

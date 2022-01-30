fb4d88a4093527422dc49902dfa0590fb7793a1f
True <levyt2@uccs.edu>
2022-01-29 22:53:07 -0700

    Item UI

---
+++ Way Past Bedtime/Assets/Scripts/ItemUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUI : MonoBehaviour
+{
+    [SerializeField] GameObject itemSlot;
+    [SerializeField] Transform itemHolder;
+
+    public void AddItem(Sprite sprite)
+    {
+        GameObject newItemSlot = Instantiate(itemSlot);
+        newItemSlot.GetComponentsInChildren<Image>()[1].sprite = sprite;
+
+        newItemSlot.transform.SetParent(itemHolder);
+
+        GetComponent<Canvas>().enabled = true;
+    }
+
+    private void Start()
+    {
+        GetComponent<Canvas>().enabled = false;
+    }
+}

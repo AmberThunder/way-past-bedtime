94f99629c0c54436e017e105da94d4f49c140b96
True <levyt2@uccs.edu>
2022-01-29 23:45:48 -0700

    turned off the light

---
+++ Way Past Bedtime/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    /* Needed sounds
+     * footsteps (player, bear, fort) (thomas)
+     * item collection
+     * battery depleted (thomas)
+     * flashlight on & off (thomas)
+     * throw pillow, pillow impact
+     * player damage
+     * baseball swing
+     * baseball hit (stock bonk sfx)
+     * win jingle
+     * music (thomas)
+     * button click
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

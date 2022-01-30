61286b404a41f0092e591eb2a347d3c737252eec
True <levyt2@uccs.edu>
2022-01-29 19:30:01 -0700

    Player takes damage and dies

---
+++ Way Past Bedtime/Assets/PlayerDamage.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class PlayerDamage : MonoBehaviour
+{
+    Light2D[] lights;
+    float fadeAmount = 0.1f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Spooky")
+        {
+            Debug.Log("spooky");
+
+            ///health--;
+            //if (health <= 0) { StartCoroutine(OnPlayerFaint()); }
+            StartCoroutine(OnPlayerFaint());
+            Destroy(collision.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = FindObjectsOfType<Light2D>();
+
+        
+    }
+
+    IEnumerator OnPlayerFaint()
+    {
+        FindObjectOfType<FlashLightController>().batteryLife = 0;
+        GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
